40d82b21debb42de9aa05720a6089555a1783dd5
DiegoArmandoMaratea <141443270+DiegoArmandoMaratea@users.noreply.github.com>
2023-12-15 16:38:10 -0300

    Create Evaluacion_BenincasaBruno.h

---
+++ Evaluacion_BenincasaBruno.h
@@ -0,0 +1,11 @@
+#ifdef Evaluacion.h
+#define Evaluacion.h 
+
+void Multiplexor_init();
+void Multiplexor();
+void Pulsador();
+void ContadorAcen();
+void ContadorDecen();
+void ControlLeds();
+
+#endif 
